6d24861e496927d3aab5bdf78614c8fb594f251a
codewithamans <a.jagat159753@gmail.com>
2023-04-10 01:03:18 +0530

    smart contract fixes

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -11,13 +11,14 @@ contract CrowdFunding {
     uint256 target;
     address Owner;
     uint256 amountCollected;
+       uint[] donations;
     uint deadline;
     address[] donators;
   }
 
   mapping(uint256=>Campaign) public campaigns;
   uint public noOfCampaigns = 0;
-
+// 1. Function for creating campaigns
  function createCampaign(address owner, string memory _title, string memory _desc,uint256 _target,uint _deadline) public returns(uint256){
 Campaign storage campaign = campaigns[noOfCampaigns];
 require(_deadline>block.timestamp,"Deadline time should be of future");
@@ -30,11 +31,28 @@ noOfCampaigns+=1;
 return noOfCampaigns -1;
  }
 
+// 2. Donate to particular campaigns
 function donateToCampaign(uint256 id) public payable {
 uint256 amount = msg.value;
 Campaign storage campaign = campaigns[id];
 campaign.donators.push(msg.sender);
+campaign.donations.push(amount);
 campaign.amountCollected+= amount;
 
+}
+
+// 3. Get all donators and donations
+function getDonators(uint256 id) public view returns (address[] memory,uint256[] memory){
+return (campaigns[id].donators,campaigns[id].donations);
+}
+
+// 4. Get all campaigns by making array since memory are not directly accessible
+function getCampaign() public view returns(Campaign[] memory){
+    Campaign[] memory allCampaigns = new Campaign[](noOfCampaigns);
+    for (uint i =0; i<noOfCampaigns;i++){
+        Campaign storage item = campaigns[i];
+        allCampaigns[i] = item;
+    }
+    return allCampaigns;
 }
 }
