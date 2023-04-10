65e7937ac140e7cf62cf9621e76e7d20a2774c18
codewithamans <a.jagat159753@gmail.com>
2023-04-10 18:05:51 +0530

    web3 integration done

--- context/CrowdFunding.js
+++ context/CrowdFunding.js
@@ -2,3 +2,37 @@ import { useState, useEffect } from "react";
 import { crowdFundingAddress, crowdFundingABI } from "./constants";
 import { ethers } from "ethers";
 import Web3modal from "web3modal";
+
+const fetchContract = (provider) => {
+  new ethers.Contract(crowdFundingAddress, crowdFundingABI, provider);
+};
+
+export const crowdFundingContext = React.createContext();
+
+export const crowdFundingProvider = ({ children }) => {
+  const title = "Crowd Funding Contract";
+  const [currentAccount, setcurrenAccount] = useState("");
+
+  const createCampaign = async (Campaign) => {
+    const { title, description, amount, deadline } = Campaign;
+    const web3 = new Web3modal();
+    const connection = await web3.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = fetchContract(signer);
+
+    try {
+      const transaction = await contract.createCampaign(
+        currentAccount,
+        title,
+        description,
+        ethers.utils.parseUnits(amount, 18),
+        new Date(deadline).getTime()
+      );
+      await transaction.wait();
+      console.log("Success !!!!", transaction);
+    } catch (error) {
+      console.log("Failure : ", error);
+    }
+  };
+};
