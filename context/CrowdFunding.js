f0cc58449382c1b16c93aeb2deaf3d2d3e7064e1
codewithamans <a.jagat159753@gmail.com>
2023-04-10 17:16:27 +0530

    smart contract deployed

---
+++ context/CrowdFunding.js
@@ -0,0 +1,4 @@
+import { useState, useEffect } from "react";
+import { crowdFundingAddress, crowdFundingABI } from "./constants";
+import { ethers } from "ethers";
+import Web3modal from "web3modal";
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,24 +1,17 @@
 const hre = require("hardhat");
 
 async function main() {
-  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  // const unlockTime = currentTimestampInSeconds + 60;
-
-  // const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
   const CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
-  const crowdFunding = await CrowdFunding.deploy(unlockTime, {
-    value: lockedAmount,
-  });
+  const crowdFunding = await CrowdFunding.deploy();
 
   await crowdFunding.deployed();
 
-  console.log(`CrowdFunding  deployed to ${CrowdFunding.address}`);
+  console.log(`CrowdFunding  deployed to ${crowdFunding.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+// Address :  0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
